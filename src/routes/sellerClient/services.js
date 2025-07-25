5b28b3b907bc0e973790d89904697e2c78f41b55
Keidevk <keinnerv11@gmail.com>
2025-07-25 00:32:22 -0400

    Cambio de services para las peticiones de pedidos

--- src/routes/sellerClient/services.js
+++ src/routes/sellerClient/services.js
@@ -1,21 +1,30 @@
 // services/StatsService.js
-class StatsService {
-  constructor(fastify) {
-    this.fastify = fastify;
-  }
+import { prisma } from "../../plugins/db.js";
 
-  async getWeeklyStats(shopId, startDate, endDate) {
-    return await this.fastify.prisma.$queryRawUnsafe(`
-      SELECT 
-        EXTRACT(DOW FROM "fechaPedido") AS day_of_week,
-        SUM("total") AS total
-      FROM "order"
-      WHERE "tiendaId" = ${shopId}
-      AND "fechaPedido" BETWEEN '${startDate.toISOString()}' AND '${endDate.toISOString()}'
-      GROUP BY day_of_week
-      ORDER BY day_of_week ASC;
-    `);
-  }
+export class StatsService {
+  constructor(fastify) {}
+
+  async getWeeklyStats(shopId,startDate,endDate) {
+    console.log(shopId,startDate,endDate)
+    return await prisma.$queryRaw`
+  SELECT 
+    EXTRACT(DOW FROM "fecha") AS day_of_week,
+    SUM("total") AS total
+  FROM "Pedido"
+  WHERE "tiendaId" = ${shopId}
+  AND "fecha" BETWEEN ${start} AND ${end}
+  GROUP BY day_of_week
+  ORDER BY day_of_week ASC;`    // return await prisma.$queryRawUnsafe(`
+    //   SELECT 
+    //     EXTRACT(DOW FROM "fecha") AS day_of_week,
+    //     SUM("total") AS total
+    //   FROM "Pedido"
+    //   WHERE "tiendaId" = ${shopId}
+    //   AND "fecha" BETWEEN ${startDate} AND ${endDate}
+    //   GROUP BY day_of_week
+    //   ORDER BY day_of_week ASC;
+    // `);
+}
 
   async getMonthlyStats(shopId, startDate, endDate) {
     return await this.fastify.prisma.$queryRawUnsafe(`
@@ -44,4 +53,4 @@ class StatsService {
   }
 }
 
-export default StatsService;
+// export default StatsService;
